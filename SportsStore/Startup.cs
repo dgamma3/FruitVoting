7ceac6593544a49dc6f2d6cff499d97273ecf93c
daniel brazil <daniel brazil@DESKTOP-1ADR2GP>
2018-10-18 16:45:23 +1000

    refactored, also published!, new base

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -22,68 +22,36 @@ namespace SportsStore {
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services) {
+           
             services.AddDbContext<AppIdentityDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration["Data:SportStoreIdentity:ConnectionString"]));
-
-            services.AddDbContext<AppIdentityDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration["Data:SportStoreIdentity:ConnectionString"]));
+                    Configuration["Data:Identity:ConnectionString"]));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<AppIdentityDbContext>()
                 .AddDefaultTokenProviders();
             services.AddTransient<IProductRepository, EFProductRepository>();
-           //services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc();
-            services.AddMemoryCache();
-            services.AddSession();
+
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsProduction()) {
-                app.UseDeveloperExceptionPage();
-                app.UseStatusCodePages();
-           } else {
-                app.UseExceptionHandler("/Error");
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
 
             app.UseStaticFiles();
-            app.UseSession();
+          
             app.UseAuthentication();
             app.UseMvc(routes => {
-            //    routes.MapRoute(name: "Error", template: "Error",
-            //        defaults: new { controller = "Error", action = "Error" });
+           
 
-                routes.MapRoute(
-                    name: null,
-                    template: "{category}/Page{productPage:int}",
-                    defaults: new { controller = "Product", action = "List" }
-                );
-
-                routes.MapRoute(
-                    name: null,
-                    template: "Page{productPage:int}",
-                    defaults: new { controller = "Product", action = "List", productPage = 1 }
-                );
-
-                routes.MapRoute(
-                    name: null,
-                    template: "{category}",
-                    defaults: new { controller = "Product", action = "List", productPage = 1 }
-                );
-
-                routes.MapRoute(
-                    name: null,
-                    template: "",
-                    defaults: new { controller = "Product", action = "List", productPage = 1 });
-
-                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+                routes.MapRoute(name: null, template: "{controller}/{action}",
+                     defaults: new { controller = "Home", action = "Index", productPage = 1 });
             });
 
-            //SeedData.EnsurePopulated(app);
-     //       await IdentitySeedData.EnsurePopulated(app);
+ 
         }
     }
 }
