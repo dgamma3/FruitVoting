92b16e9ff972f1a85f6fe618568506c359d588e4
daniel brazil <daniel brazil@DESKTOP-1ADR2GP>
2018-10-18 16:03:14 +1000

    removed unsed files

--- SportsStore/Models/AppIdentityDbContext.cs
+++ SportsStore/Models/AppIdentityDbContext.cs
@@ -10,6 +10,5 @@ namespace SportsStore.Models {
             : base(options) { }
 
         public DbSet<Product> Products { get; set; }
-        public DbSet<Order> Orders { get; set; }
     }
 }
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -36,7 +36,6 @@ namespace SportsStore {
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddMvc();
             services.AddMemoryCache();
             services.AddSession();
